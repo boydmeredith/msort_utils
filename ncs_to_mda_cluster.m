6aa81e66f7a2822fec66d021d3d9e528070c7900
Tyler Boyd-Meredity <jtb3@spock-login.pni.Princeton.EDU>
2020-06-21 21:47:23 -0400

    fix ncs_to_mda_cluster directory handling

--- ncs_to_mda_cluster.m
+++ ncs_to_mda_cluster.m
@@ -1,10 +1,14 @@
 function mda = ncs_to_mda_cluster(expname, ratname, sessiondate, tt, savedir)
 % function ncs_to_mda_cluster(expname, ratname, sessiondate, tt, savedir)
-
+expname
+ratname
+sessiondate
+tt
+savedir
 % determine where to find the data
 physdata_dir = '/jukebox/brody/physdata/';
 rat_fldr = fullfile(physdata_dir,expname, ratname);
-results = dir([rat_fldr, sessiondate, '*']);
+results = dir(fullfile(rat_fldr, [sessiondate, '*']));
 
 if isempty(results)
     error('Problem finding data folder: No session found');
@@ -12,25 +16,29 @@ elseif length(results) > 1
     error('Problem finding data folder: Multiple sessions found')
 end
 
-fldr_data = [rat_fldr,results.name,'/'];
-fldr_scripts = '/jukebox/brody/kjmiller/Mountainsort/';
+fldr_data = fullfile(rat_fldr,results.name);
+% this folder will get us mdaio stuff as well as nlx stuff
+fldr_scripts = '/jukebox/brody/kjmiller/Mountainsort/Utilities';
 addpath(genpath(fldr_scripts));
 addpath('/usr/jtb3/code/msort_utils')
 
 % Nlx2MatCSC_v3 is the version for Linux
 fprintf(['Checking for data in: ', fldr_data,' \n', 'Loading data header... \n'])
-hd = Nlx2MatCSC_v3([fldr_data,'TT', tt, '_1.ncs'],[0,0,0,0,0],1,1)
+tt_fn = @(cc) fullfile(fldr_data,['TT',tt,'_' num2str(cc) '.ncs']);
+hd = Nlx2MatCSC_v3(tt_fn(1),[0,0,0,0,0],1,1)
 
-temp = strfind(hd,'-SamplingFrequency') + length(srch_str);
-fs   = str2num(hd(temp-1+regexp(hd(temp:temp+10),'\d')));
+srch_str = '-SamplingFrequency'; 
+temp = find(cellfun(@isempty, strfind(hd,srch_str)) == 0);
+idx  = (regexp(hd{temp},'\d'));
+fs   = str2num(hd{temp}(idx));
     us_per_sample = 1e6 / fs;
 
 %% Check everything is ok with the session timing
 fprintf('Checking timing\n')
-ts  = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_1.ncs'],[1,0,0,0,0],0,1);
-ts2 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_2.ncs'],[1,0,0,0,0],0,1);
-ts3 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_3.ncs'],[1,0,0,0,0],0,1);
-ts4 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_4.ncs'],[1,0,0,0,0],0,1);
+ts  = Nlx2MatCSC_v3(tt_fn(1),[1,0,0,0,0],0,1);
+ts2 = Nlx2MatCSC_v3(tt_fn(2),[1,0,0,0,0],0,1);
+ts3 = Nlx2MatCSC_v3(tt_fn(3),[1,0,0,0,0],0,1);
+ts4 = Nlx2MatCSC_v3(tt_fn(4),[1,0,0,0,0],0,1);
 
 
 if length(unique(diff(ts))) ~= 1
@@ -45,7 +53,7 @@ clear ts2; clear ts3; clear ts4;
 
 fprintf('Timing looks good!\nLoading the data\n')
 %% Make the MDA
-samps1  = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_1.ncs'],[0,0,0,0,1],0,1);
+samps1  = Nlx2MatCSC_v3(tt_fn(1),[0,0,0,0,1],0,1);
 
 n = 4;
 m = numel(samps1);
@@ -55,24 +63,26 @@ mda(1,:) = samps1(:);
 fprintf('Channel 1 loaded\n')
 clear samps1;
 
-samps2 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_2.ncs'],[0,0,0,0,1],0,1);
+samps2 = Nlx2MatCSC_v3(tt_fn(2),[0,0,0,0,1],0,1);
 mda(2,:) = samps2(:); 
 fprintf('Channel 2 loaded\n')
 clear samps2;
 
-samps3 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_3.ncs'],[0,0,0,0,1],0,1);
+samps3 = Nlx2MatCSC_v3(tt_fn(3),[0,0,0,0,1],0,1);
 mda(3,:) = samps3(:); 
 fprintf('Channel 3 loaded\n')
 clear samps3;
 
-samps4 = Nlx2MatCSC_v3([fldr_data, 'TT', tt, '_4.ncs'],[0,0,0,0,1],0,1);
+samps4 = Nlx2MatCSC_v3(tt_fn(4),[0,0,0,0,1],0,1);
 mda(4,:) = samps4(:); 
 fprintf('Channel 4 loaded\n')
 clear samps4;
 
 % Write the mda
 fprintf('Writing MDA file\n')
-writemda64(mda,fullfile(savedir,[ratname, '_', sessiondate, '_TT', tt, '.mda'])
+tic
+writemda64(mda,fullfile(savedir,[ratname, '_', sessiondate, '_TT', tt, '.mda']))
+toc
 fprintf('MDA file written\n')
 
 end
